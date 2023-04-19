26ae89039a29c8bcfa390f93e35fbf28b59262f5
Ludovic Tiako <ludovic.tiako@gmail.com>
2023-04-19 08:02:08 -0400

    Add RedisCache to cache pakage

--- httpx_cache/cache/__init__.py
+++ httpx_cache/cache/__init__.py
@@ -1,9 +1,11 @@
 from httpx_cache.cache.base import BaseCache
 from httpx_cache.cache.file import FileCache
 from httpx_cache.cache.memory import DictCache
+from httpx_cache.cache.redis import Rediscache
 
 __all__ = [
     "BaseCache",
     "DictCache",
     "FileCache",
+    "RedisCache",
 ]
