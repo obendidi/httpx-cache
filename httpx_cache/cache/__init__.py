d5fdfc0ae0924efe652d42d817cc613bafc86037
Ludovic Tiako <ludovic.tiako@gmail.com>
2023-04-19 17:21:07 -0400

    Remove top level package import

--- httpx_cache/cache/__init__.py
+++ httpx_cache/cache/__init__.py
@@ -1,11 +1,9 @@
 from httpx_cache.cache.base import BaseCache
 from httpx_cache.cache.file import FileCache
 from httpx_cache.cache.memory import DictCache
-from httpx_cache.cache.redis import Rediscache
 
 __all__ = [
     "BaseCache",
     "DictCache",
     "FileCache",
-    "RedisCache",
 ]
